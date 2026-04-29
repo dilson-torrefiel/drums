3de44b9e1ba249ba96ce0ef28aaf2e97853ad0fa
dilsontorrefiel <cpe.dilsontorrefiel@gmail.com>
2026-04-30 02:08:35 +0400

    update code added jquery

--- drum-kit.js
+++ drum-kit.js
@@ -1,9 +1,5 @@
 /**
  * Drum Kit
- * This JavaScript code adds interactivity to a drum kit web application. It listens for click events on various drum images (kick, toms, and cymbals) and plays the corresponding sound when each image is clicked. The sounds are reset to the beginning before playing to allow for rapid consecutive hits.
- * Each drum element is selected using `document.querySelector`, and an event listener is attached to it. When a drum image is clicked, the associated sound's `currentTime` property is set to 0 to reset the playback position, and then the `play()` method is called to play the sound.
- * This code assumes that the HTML structure includes elements with the specified classes and corresponding audio elements with the correct IDs for the sounds.
- * Overall, this code enables users to interact with the drum kit by clicking on the images to produce the appropriate drum sounds, creating a fun and engaging musical experience.
  *
  * Author: Dilson Torrefiel
  * Date: April 2026
@@ -51,23 +47,25 @@ function animateDrum(element) {
  * Get all image elements and add event listeners to play the corresponding sound when each drum image is clicked. The function checks the class of each image element to determine which sound to play and which drum element to animate.
  * The event listener is added using the `onPointerDown` helper function, which handles the pointerdown event and plays the sound while animating the drum element.
  */
-document.querySelectorAll(".drum img").forEach((element) => {
-  if (element.classList.contains("kick-image")) {
-    onPointerDown(element, document.getElementById("kick-sound"));
-  } else if (element.classList.contains("cymbal-image")) {
-    onPointerDown(element, document.getElementById("cymbal-sound"));
-  } else if (element.classList.contains("snare-image")) {
-    onPointerDown(element, document.getElementById("snare-sound"));
-  } else if (element.classList.contains("tom1-image")) {
-    onPointerDown(element, document.getElementById("tom1-sound"));
-  } else if (element.classList.contains("tom2-image")) {
-    onPointerDown(element, document.getElementById("tom2-sound"));
-  } else if (element.classList.contains("tom3-image")) {
-    onPointerDown(element, document.getElementById("tom3-sound"));
-  } else if (element.classList.contains("tom4-image")) {
-    onPointerDown(element, document.getElementById("tom4-sound"));
-  }
-});
+$(".drum img")
+  .get()
+  .forEach((element) => {
+    if (element.classList.contains("kick-image")) {
+      onPointerDown(element, $("#kick-sound").get(0));
+    } else if (element.classList.contains("cymbal-image")) {
+      onPointerDown(element, $("#cymbal-sound").get(0));
+    } else if (element.classList.contains("snare-image")) {
+      onPointerDown(element, $("#snare-sound").get(0));
+    } else if (element.classList.contains("tom1-image")) {
+      onPointerDown(element, $("#tom1-sound").get(0));
+    } else if (element.classList.contains("tom2-image")) {
+      onPointerDown(element, $("#tom2-sound").get(0));
+    } else if (element.classList.contains("tom3-image")) {
+      onPointerDown(element, $("#tom3-sound").get(0));
+    } else if (element.classList.contains("tom4-image")) {
+      onPointerDown(element, $("#tom4-sound").get(0));
+    }
+  });
 
 /**
  * Handles Kyeboard events to play the corresponding drum sound and animate the drum element when a specific key is pressed. The keys are mapped as follows:
@@ -83,32 +81,32 @@ window.addEventListener("keydown", (event) => {
   var key = event.key.toUpperCase();
   switch (key) {
     case "A":
-      playSound(document.getElementById("snare-sound"));
-      animateDrum(document.querySelector(".snare img"));
+      playSound($("#snare-sound").get(0));
+      animateDrum($(".snare img").get(0));
       break;
     case "S":
-      playSound(document.getElementById("cymbal-sound"));
-      animateDrum(document.querySelector(".cymbal img"));
+      playSound($("#cymbal-sound").get(0));
+      animateDrum($(".cymbal img").get(0));
       break;
     case "D":
-      playSound(document.getElementById("kick-sound"));
-      animateDrum(document.querySelector(".kicks img"));
+      playSound($("#kick-sound").get(0));
+      animateDrum($(".kicks img").get(0));
       break;
     case "J":
-      playSound(document.getElementById("tom3-sound"));
-      animateDrum(document.querySelector(".tom3 img"));
+      playSound($("#tom3-sound").get(0));
+      animateDrum($(".tom3 img").get(0));
       break;
     case "K":
-      playSound(document.getElementById("tom4-sound"));
-      animateDrum(document.querySelector(".tom4 img"));
+      playSound($("#tom4-sound").get(0));
+      animateDrum($(".tom4 img").get(0));
       break;
     case "L":
-      playSound(document.getElementById("tom1-sound"));
-      animateDrum(document.querySelector(".tom1 img"));
+      playSound($("#tom1-sound").get(0));
+      animateDrum($(".tom1 img").get(0));
       break;
     case ";":
-      playSound(document.getElementById("tom2-sound"));
-      animateDrum(document.querySelector(".tom2 img"));
+      playSound($("#tom2-sound").get(0));
+      animateDrum($(".tom2 img").get(0));
       break;
     default:
       break;
